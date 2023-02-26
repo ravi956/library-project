e1325b3949a6cefa34c41c875e32bb03aa5b3882
Ravi Kumar <ravikumar956078@gmail.com>
2023-02-27 01:27:29 +0530

    Navigation bar component

--- library-project-frontend/react-library/src/App.tsx
+++ library-project-frontend/react-library/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the Library</h1>
-    </div>
+    <Navbar/>
   );
 }
 
